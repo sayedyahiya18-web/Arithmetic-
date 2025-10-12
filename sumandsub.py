87de65f17c7a6e4f181e16036af137c553fc20fb
sayedyahiya18-web <sayedyahiya18@gmail.com>
2025-10-12 22:27:42 +0530

    save this file

--- sumandsub.py
+++ sumandsub.py
@@ -4,3 +4,10 @@ sum=n1+n2
 sub=n1-n2
 print("Sum is:",sum)
 print("Subtraction is:",sub)
+mult=n1*n2
+print("Multiplication is:",mult)
+if n2!=0:
+    div=n1/n2
+    print("Division is:",div)
+else:
+    print("Division by zero is not possible")
